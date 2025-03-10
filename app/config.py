8263a48b02f4bbb416c590918d7bb19cf173e1e6
soniaJ <jsonia.infercon@gmail.com>
2025-03-10 16:15:27 +0530

    update

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
-    GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 5))
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
 
 settings = Settings()
